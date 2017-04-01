28b3e44c884877d0dadc963e19e051176bf4c687
墨尘 <1750134638@qq.com>
2017-04-01 08:54:56 +0800

    00

--- js/eteams.js
+++ js/eteams.js
@@ -2,6 +2,7 @@ $(function (){
 
 
 $("#user-con").hide();
+$(".home").hide();
 
 /***
  * 参数：
@@ -9,30 +10,32 @@ $("#user-con").hide();
  * optionId： 操作对象的ID
  * 作用： 根据传入的    绑定对象的ID 添加 获得焦点入显示，失去焦点隐藏功能
  * */
-var itemShowHide = function(selfId,optionId){
+var itemShowHide = function(selfId,optionId,a){
 //	alert(selfId+ "---------"+optionId);
 	
 	$(selfId).mouseover(function(){
 		$(optionId).show();
-
-	});		
+	});	
 	$(selfId).mouseout(function(){
 		$(optionId).hide();
 	});	
-	itemShowHide(optionId,optionId);
+	if(a===1){
+		$(optionId).mouseover(function(){
+			$(optionId).show();
+		});	
+		$(optionId).mouseout(function(){
+			$(optionId).hide();
+		});	
+		
+	}
 }
 
-itemShowHide(".user-div","#user-con");
+itemShowHide(".user-div","#user-con",1);
+itemShowHide(".home-div",".home",1);
+
 
 //   默认时隐藏全部菜单
 
 
 
-
-	
-	
-	
-	
-	
-	
 });
