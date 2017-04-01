0fc0d65a2ecdd1d65d9028311ba232c8fcda9abb
墨尘 <1750134638@qq.com>
2017-04-01 18:15:54 +0800

    123

--- js/eteams.js
+++ js/eteams.js
@@ -1,13 +1,22 @@
 $(function (){
-
-
+	
+	
+	
+	var home = document.getElementById("home");
+	var usercon = document.getElementById("user-con");
+	usercon.style.display="none";
+	home.style.display="none";
+	
+	
 $("#user-con").hide();
 $(".home").hide();
-
+$(".hidebody").hide();
+$(".modifypass").hide();
 /***
  * 参数：
  * selfId： 绑定对象的ID
  * optionId： 操作对象的ID
+ * sel 
  * 作用： 根据传入的    绑定对象的ID 添加 获得焦点入显示，失去焦点隐藏功能
  * */
 var itemShowHide = function(selfId,optionId,a){
@@ -25,17 +34,56 @@ var itemShowHide = function(selfId,optionId,a){
 		});	
 		$(optionId).mouseout(function(){
 			$(optionId).hide();
-		});	
-		
+		});		
 	}
 }
-
 itemShowHide(".user-div","#user-con",1);
 itemShowHide(".home-div",".home",1);
 
 
-//   默认时隐藏全部菜单
+$("#modpass").click(function (){
+//	$(".hidebody").
+	$(".hidebody").show();
+	$(".modifypass").show();
+});
+$("#abolish").click(function (){
+	$(".hidebody").hide();
+	$(".modifypass").hide();
+});
 
+$(".left-gps").css({height:$(document).height()});
+$(".hidebody").css({height:$(document).height()});
+
+
+// 浏览器兼容 取得浏览器可视区高度   
+function getWindowInnerHeight() {   
+    var winHeight = window.innerHeight   
+            || (document.documentElement && document.documentElement.clientHeight)   
+            || (document.body && document.body.clientHeight);   
+    return winHeight;   
+       
+}   
+  
+// 浏览器兼容 取得浏览器可视区宽度   
+function getWindowInnerWidth() {   
+    var winWidth = window.innerWidth   
+            || (document.documentElement && document.documentElement.clientWidth)   
+            || (document.body && document.body.clientWidth);   
+    return winWidth;   
+       
+}   
+var changeColor = function(selId){
+	$(selId).click(function (){
+	//	alert(selId);
+		//$(selId).css({border-bottom:"1px",border-color:"red"});
+		
+	});
+	
+};
+
+changeColor("#basic-information");
+
+changeColor("#subordinate");
 
 
 });
